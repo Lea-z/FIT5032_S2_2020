7e71d3cdf7980cdf1c5497271ddf89a82278e983
Lea-z <52340606+Lea-z@users.noreply.github.com>
2020-08-19 13:39:00 +1000

    Add files via upload
    with the student part

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FIT5032_week03.Exercise;
 using FIT5032_week03.HelloWorld;
 
 namespace FIT5032_week03.Controllers
@@ -19,6 +20,9 @@ namespace FIT5032_week03.Controllers
             ViewBag.Message = "Your application description page.";
             Hello hello = new Hello();
             ViewBag.Message = hello.GetHello();
+
+            ExampleDictionary ed = new ExampleDictionary();
+            ed.Example();
 
             return View();
         }
