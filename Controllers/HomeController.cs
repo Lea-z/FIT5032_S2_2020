a79ff19ab0326ee9839b3d4887dd1295424d91ee
Lea-z <52340606+Lea-z@users.noreply.github.com>
2020-08-19 12:57:21 +1000

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FIT5032_week03.HelloWorld;
+
+namespace FIT5032_week03.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            Hello hello = new Hello();
+            ViewBag.Message = hello.GetHello();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
